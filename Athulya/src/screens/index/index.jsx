ab57600d2282271877adea8f133c4b05aa1dd434
athulyar99 <99779659+athulyar99@users.noreply.github.com>
2023-11-19 16:11:12 +0530

    main page full source code added

---
+++ Athulya/src/screens/index/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="overlap-group">
+          <img
+            className="night-falls-spooky"
+            alt="Night falls spooky"
+            src="https://cdn.animaapp.com/projects/6559c3825c6faf1eb176e5e9/releases/6559c83b7a5f7fd6b9fd156e/img/night-falls-spooky-gothic-skyscraper-generated-by-ai-1.png"
+          />
+          <div className="rectangle" />
+          <img
+            className="element-sorting"
+            alt="Element sorting"
+            src="https://cdn.animaapp.com/projects/6559c3825c6faf1eb176e5e9/releases/6559c83b7a5f7fd6b9fd156e/img/106-1067907-sorting-hat-png-harry-potter-sorting-hat-png-1.png"
+          />
+          <div className="frame">
+            <div className="text-wrapper">Name</div>
+            <div className="text-wrapper-2">E Number</div>
+            <div className="text-wrapper-3">Group</div>
+          </div>
+          <div className="text-wrapper-4">Welcome to Electrolizer ‘23</div>
+          <div className="rectangle-2" />
+          <div className="rectangle-3" />
+          <div className="rectangle-4" />
+          <div className="rectangle-5" />
+        </div>
+        <div className="overlap">
+          <img
+            className="logo-eees-main"
+            alt="Logo eees main"
+            src="https://cdn.animaapp.com/projects/6559c3825c6faf1eb176e5e9/releases/6559c83b7a5f7fd6b9fd156e/img/logo-eees-main-1.png"
+          />
+          <img
+            className="DEEE-LOGO-lr"
+            alt="Deee LOGO lr"
+            src="https://cdn.animaapp.com/projects/6559c3825c6faf1eb176e5e9/releases/6559c83b7a5f7fd6b9fd156e/img/deee-logo-lr-0-1.png"
+          />
+          <div className="text-wrapper-5">ELECTROLIZER ‘23</div>
+        </div>
+      </div>
+    </div>
+  );
+};
