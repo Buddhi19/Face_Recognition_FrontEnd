c42e0a1a0ecb1729389e61e64ac3bd837d7089da
Buddhi Wijenayake <wijenayakebuddhi34802@gmail.com>
2023-11-21 01:28:07 +0530

    I dont know what i did

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import Header from "./components/Header"
+import LeftPane from "./components/LeftPane"
 import './App.css';
 import axios from 'axios';
 import Webcam from 'react-webcam';
-import * as faceapi from 'face-api.js';
 
-const WebcamComponent = () => <Webcam />;
 
-const App = () => {
+const Profile = () => {
   const [picture, setPicture] = useState('');
   const [responseText, setResponseText] = useState('');
 
-  const webcamRef = useRef(null);
+  const webcamRef = React.useRef(null);
 
-  const capture = async () => {
+  const capture = React.useCallback(async () => {
     const pictureSrc = webcamRef.current.getScreenshot();
     console.log(pictureSrc);
 
@@ -22,8 +22,8 @@ const App = () => {
 
       const response = await axios.post('http://192.168.252.9:5000/emotion', formData, {
         headers: {
-          'Content-Type': 'multipart/form-data',
-        },
+          'Content-Type': 'multipart/form-data'
+        }
       });
 
       console.log('Image successfully posted');
@@ -34,53 +34,52 @@ const App = () => {
     } catch (error) {
       console.error('Failed to post image:', error);
     }
-  };
+  }, []);
 
   const handleRetake = () => {
     setPicture('');
     setResponseText('');
   };
 
-  // Use setInterval to capture images every second (1000 milliseconds)
-  useEffect(() => {
-    const intervalId = setInterval(capture, 1000);
-
-    // Clear the interval when the component unmounts
-    return () => clearInterval(intervalId);
-  }, []);
-
   return (
-    <div className="container">
-      <p className="title">Hello World</p>
-
-      <div className="webcam-container">
+    <div className="background-container">
+      <img src='/moon2.png'/>
+      <div className='stars'/>
+      <div className='twinkling'/>
+      <div className='clouds'/>
+      {/* <StarBackground/> */}
+      <Header />
+      <LeftPane />
+      <div className='webcam-container'>
+      <div className="webapp-item">
         {picture === '' ? (
           <Webcam
             mirrored={true}
             audio={false}
-            height={400}
+            height={800}
             ref={webcamRef}
-            width={400}
+            width={750}
             screenshotFormat="image/jpg"
-          />
-        ) : (
+          />): (
           <img src={picture} alt="Captured" />
         )}
       </div>
-      <div className="button-container">
+      </div>
+      <div className="photo-button">
+        <div className='ring'/><div className='circle'/>
         {picture !== '' ? (
           <>
-            <button className="button" onClick={handleRetake}>
-              Retake
-            </button>
+            <button className="button2" onClick={handleRetake}/>
           </>
-        ) : null /* Remove the Capture button */}
+        ) : (
+          <button className="button" onClick={capture}/>
+        )}
       </div>
-      <div className="title">
-        {responseText && <p className="response">{JSON.parse(responseText).emotion}</p>}
+      <div className='title'>
+      {responseText && <p className="response">{JSON.parse(responseText).emotion}</p>}
       </div>
     </div>
   );
 };
 
-export default App;
+export default Profile;
---
+++ src/components/start.js
@@ -0,0 +1,19 @@
+// StarBackground.js
+import React from 'react';
+import './StarBackground.css';
+
+const StarBackground = () => {
+  return (
+ <div>
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      <div id="title">
+        <span>PURE CSS</span>
+        <br />
+        <span>PARALLAX PIXEL STARS</span>
+      </div>
+    </div>
+  );
+}
+export default StarBackground;
